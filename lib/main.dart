4e0f4e892d147789941f864cb3bc7818266cd3e7
Mimoriam <mimoriam420@gmail.com>
2021-06-11 20:31:56 +0500

    Commit #2 - Finally some good fucking progress!

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,19 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart';
+
 import 'package:shared_preferences/shared_preferences.dart';
 
 // ignore: implementation_imports
 import 'package:docx_template/src/template.dart';
+
 // ignore: implementation_imports
 import 'package:docx_template/src/model.dart';
 
 import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:desktop_window/desktop_window.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,8 +30,16 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  Future<void> toggleFullScreen() async {
+    await DesktopWindow.toggleFullScreen();
+    bool isFullScreen = await DesktopWindow.getFullScreen();
+
+    await DesktopWindow.setFullScreen(true);
+  }
+
   @override
   Widget build(BuildContext context) {
+    toggleFullScreen();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: AnimatedSplashScreen(
@@ -67,7 +80,13 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     fileCounterAsync();
+    createRequiredFolders();
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 
   void fileCounterAsync() async {
@@ -76,12 +95,49 @@ class _MyHomePageState extends State<MyHomePage> {
 
   final _formKey = GlobalKey<FormBuilderState>();
 
-  // void dateChanged(DateTime? dt) {
-  //   // DateTime? at = _formKey.currentState?.fields['date']?.value;
-  // }
+  Future<void> dateChanged(DateTime? dt) async {
+    // DateTime? at = _formKey.currentState?.fields['date']?.value;
+  }
+
+  Future<void> createRequiredFolders() async {
+    final dataDir = Directory.current.path;
+    final Directory dataDirFolder = Directory("${dataDir}\\data\\");
+    final File myFile = File("${dataDir}\\data\\template.docx");
+
+    final Directory appDocDir = await getApplicationDocumentsDirectory();
+    final Directory appDocDirFolder = Directory("${appDocDir.path}\\MNSUET Degrees\\");
+
+    if (await appDocDirFolder.exists()) {
+      print("${appDocDirFolder.path} exists ~!");
+    } else {
+      final Directory appDocDirNewFolder = await appDocDirFolder.create(recursive: true);
+    }
+
+    final Directory templateDirFolder = Directory("${appDocDir.path}\\MNSUET Degrees\\Template\\");
+
+    if (await templateDirFolder.exists()) {
+      print("${templateDirFolder.path} exists!");
+    } else {
+      final Directory templateDirNewFolder = await templateDirFolder.create(recursive: true);
+    }
+
+    final Directory generatedDirFolder =
+        Directory("${appDocDir.path}\\MNSUET Degrees\\Generated\\");
+
+    if (await generatedDirFolder.exists()) {
+      print("${generatedDirFolder.path} exists!");
+    } else {
+      final Directory generatedDirNewFolder = await generatedDirFolder.create(recursive: true);
+    }
+
+    if (await dataDirFolder.exists()) {
+      myFile.copy('${templateDirFolder.path}\\template.docx');
+    }
+  }
 
   Future<void> saveFileToDisk() async {
-    final f = File("C:\\template.docx");
+    final Directory appDocDir = await getApplicationDocumentsDirectory();
+    final f = File("${appDocDir.path}\\MNSUET Degrees\\Template\\template.docx");
     final docx = await DocxTemplate.fromBytes(await f.readAsBytes());
     Content c = Content();
     DateTime? dt = _formKey.currentState?.fields['date']?.value;
@@ -106,9 +162,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
     c
       ..add(TextContent("year", "${dt.year}"))
-      ..add(TextContent("CGPA", "${_formKey.currentState?.fields['CGPA']?.value}"))
-      ..add(TextContent("degree_level2", "${_formKey.currentState?.fields['degree_level']?.value}"))
-      ..add(TextContent("degree_title", "${_formKey.currentState?.fields['degree_title']?.value}"));
+      ..add(TextContent("CGPA", "${_formKey.currentState?.fields['CGPA']?.value}"))..add(
+        TextContent(
+            "degree_level2", "${_formKey.currentState?.fields['degree_level']?.value}"))..add(
+        TextContent("degree_title", "${_formKey.currentState?.fields['degree_title']?.value}"));
 
     if (_formKey.currentState?.fields['honors']?.value == true) {
       c..add(TextContent("honours", "with Honors"));
@@ -117,12 +174,20 @@ class _MyHomePageState extends State<MyHomePage> {
     }
 
     final d = await docx.generate(c);
-    final of = File('C:\\generated${context.read(fileCounterProvider).state}.docx');
+    final of = File(
+        "${appDocDir.path}\\MNSUET Degrees\\Generated\\degree${context
+            .read(fileCounterProvider)
+            .state}.docx");
+    // final of = File('C:\\generated${context.read(fileCounterProvider).state}.docx');
 
     if (d != null) {
       await of.writeAsBytes(d);
-      context.read(fileCounterProvider).state++;
-      await persistFileCounter(context.read(fileCounterProvider).state);
+      context
+          .read(fileCounterProvider)
+          .state++;
+      await persistFileCounter(context
+          .read(fileCounterProvider)
+          .state);
     }
   }
 
@@ -151,8 +216,9 @@ class _MyHomePageState extends State<MyHomePage> {
                       width: MediaQuery.of(context).size.width * 0.01,
                     ),
                     Text(
-                      "MNSUET!",
+                      "Muhammad Nawaz Sharif \nUniversity of Engineering\n & Technology, Multan",
                       style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.width * 0.014,
                         color: Colors.white,
                       ),
                     ),
@@ -228,7 +294,7 @@ class _MyHomePageState extends State<MyHomePage> {
                             name: 'date',
                             inputType: InputType.date,
                             format: DateFormat('dd-MM-yyyy'),
-                            // onChanged: dateChanged,
+                            onChanged: dateChanged,
                             decoration: InputDecoration(
                               labelText: 'Date',
                               border: OutlineInputBorder(
